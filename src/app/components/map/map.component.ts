001136acc9b3b17f2b4c1e858b1dbafab3b1f489
SJ <serge.szynkarski@le-campus-numerique.fr>
2024-09-11 18:35:44 +0200

    Added markers and Grenoble data

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
+import { locations } from '../../../assets/locations';
 
 @Component({
   selector: 'app-map',
@@ -15,15 +16,36 @@ export class MapComponent implements OnInit {
     this.initMap();
   }
 
+  private calculateCenter(locations: [number, number][]): [number, number] {
+    let sumLat = 0;
+    let sumLng = 0;
+
+    locations.forEach(location => {
+      sumLat += location[0];
+      sumLng += location[1];
+    });
+
+    const avgLat = sumLat / locations.length;
+    const avgLng = sumLng / locations.length;
+
+    return [avgLat, avgLng];
+  }
+
   private initMap(): void {
+    const center = this.calculateCenter(locations);
+
     this.map = L.map('map', {
-      center: [51.505, -0.09],
-      zoom: 2
+      center: center,
+      zoom: 13
     });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19
     }).addTo(this.map);
+
+    locations.forEach((location) => {
+      L.marker([location[0], location[1]]).addTo(this.map);
+    });
   }
 
 }
