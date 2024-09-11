bc6b2a48ecc81073f3d639e39bbdfaf28910142f
SJ <serge.szynkarski@le-campus-numerique.fr>
2024-09-11 18:16:58 +0200

    Added Map component

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit {
+  private map: any;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [51.505, -0.09],
+      zoom: 2
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19
+    }).addTo(this.map);
+  }
+
+}
